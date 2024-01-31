e9e12446396d0f6cf31d09b499da13c97e9c14b2
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2024-01-30 22:34:40 -0800

    update cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require('./passport');
 app.use(express.static('public'));
 
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://dudasnewmyflixapp.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://dudasnewmyflixapp.netlify.app', 'http://localhost:4200'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
